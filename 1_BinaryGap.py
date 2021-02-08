2da0c0f42ab7b7d47e94542d06f6b2c11eb7609a
jiri.one <nemec@jiri.one>
2021-02-08 19:50:33 +0100

    first commit with first solution

---
+++ 1_BinaryGap.py
@@ -0,0 +1,11 @@
+def solution(N):
+	string_of_bin = str(bin(N))[2:].split("1")
+	if "0" in string_of_bin[-1]:
+		string_of_bin.pop(-1)
+	zeros = [x for x in string_of_bin if x]
+	if len(zeros) == 0:
+		return 0
+	zeros.sort(key=len, reverse=True)
+	return len(zeros[0])
+
+print(solution(51712))
